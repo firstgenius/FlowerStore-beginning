5a68f923ef090098cc462a0fd1960e2cb986a86f
firstgenius <73236714+firstgenius@users.noreply.github.com>
2021-10-25 22:52:19 +0300

    Add files via upload

---
+++ src/main/java/flowerstore/Store.java
@@ -0,0 +1,21 @@
+package flowerstore;
+
+public class Store {
+    public int amountRoses = 143;
+    public int amountTulips = 15;
+    public int amountChamomiles = 35;
+
+    // search returns whether store has enough needed flowers to complete ur order
+    public boolean search(Flower flower) {
+        if (flower.getFlowerType().equals(FlowerType.CHAMOMILE)){
+            return (flower.getAmount() <= amountChamomiles);
+        }
+        if (flower.getFlowerType().equals(FlowerType.ROSE)){
+            return (flower.getAmount() <= amountRoses);
+        }
+        if (flower.getFlowerType().equals(FlowerType.TULIP)){
+            return (flower.getAmount() <= amountTulips);
+        }
+        return false;
+    }
+}
